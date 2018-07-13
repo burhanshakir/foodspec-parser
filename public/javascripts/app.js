e013646c63e4379a1b97eb5680456134e24bc4cd
bshakir <burhanshakir96@gmail.com>
2018-07-13 15:38:14 +0100

    Reading pdf files

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,13 +2,30 @@ var app = angular.module('foodspec-parser', []);
 
 app.controller('receivePdfController', function ($scope) {
 
+    $scope.formData = {};
+    $scope.formData.file = "";
 
     $scope.pdfSubmit = () =>
     {
+        console.log($scope.formData.email);
         console.log($scope.formData);
 
     }
 
 
-
 });
+
+
+app.directive("selectNgFiles", [function () {
+
+    return {
+        require: "ngModel",
+        link: function postLink(scope,elem,attrs,ngModel) {
+            elem.on("change", function(e) {
+                var files = elem[0].files;
+                ngModel.$setViewValue(files);
+            })
+        }
+    }
+    
+}]);
